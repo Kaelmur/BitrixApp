3c17982cf0047c28d05e3ecc9f7552cc15b8b9e7
Kaelmur <a.murataliev@list.ru>
2025-09-23 21:14:13 +0500

    added type for User

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import db from "@/lib/db";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { User } from "@/types";
 
 export async function POST(req: Request) {
   try {
